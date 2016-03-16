48cc6873b5a34a698174619248eea92a76887e2a
Metin DENİZ <mmismail@anadolu.edu.tr>
2016-03-17 00:37:15 +0200

    marker takipli

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -16,6 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+var map;
 var app = {
     // Application Constructor
     initialize: function() {
@@ -33,9 +34,69 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-      
-    }
+          map =  document.getElementById('haritaFrameId').contentWindow.harita();
+          map.cihazinYerinigoster();
+         yerTakipEt();
+    } 
    
+};
+
+function yerTakipEt() {
+
+    if (map == undefined) {
+        setInterval(yerTakipEt, 1000);
+        console.log("map yok");
+
+        return;
+    }
+    var options = { maximumAge: 0, timeout: 10000, enableHighAccuracy: true };
+    navigator.geolocation.watchPosition(yerBlgisiBsariliGuncellendi, yerTakipEdilememeDurumu, options);
+}
+
+function yerTakipEdilememeDurumu(error) {
+    alert(" yer biildirilemedi" + error);
+}
+
+ 
+
+var yerBlgisiBsariliGuncellendi = function (position) {
+
+    var coords = position.coords;
+    var latitudeYeni = coords.latitude;
+
+    var longitudeYeni = coords.longitude;
+    var yatayDeger = latitudeYeni - coords.latitude;
+    var sapmaDegeri = 0.00001;
+    var yatayDegisimis = -sapmaDegeri < yatayDeger && yatayDeger < sapmaDegeri;
+    var dikeyDeger = longitudeYeni - coords.longitude;
+    var dikeyDegisimis = -sapmaDegeri < dikeyDeger && dikeyDeger < sapmaDegeri;
+    //if (!(coords && yatayDegisimis && dikeyDegisimis)) {
+    //
+    //    console.log("kordinat deðiþmedi");
+    //    return;
+    //}
+    var cihazPozisyon = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+    };
+    map.cihazPozisyonGuncelle(cihazPozisyon);
+  //  console.log("kordinat deðiþti");
+    positionGen = position;
+    var label = device.platform + " - " + device.model;
+    // construct an HTTP request
+    var data = {
+        latitude: latitudeYeni,
+        longitude: longitudeYeni,
+        altitude: position.coords.altitude,
+        accuracy: position.coords.accuracy,
+        altitudeAccuracy: position.coords.altitudeAccuracy,
+        speed: position.coords.speed,
+   
+        telefonLabel: label
+    };
+
+   // var json = httpPost("hello/lokasyon/guncelle", data);
+    //cihazlariGuncelle(json);
 };
 
 app.initialize();
--- www/js/harita.js
+++ www/js/harita.js
@@ -1,43 +1,83 @@
-﻿var map;
-var markers = [];
-function initMap() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            var cihazPozisyon = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
-            map = new google.maps.Map(document.getElementById('map'), {
-                center: cihazPozisyon,
-                disableDefaultUI: true,
-                zoom: 17
-            });
-            cihazPozisyonAyarla(cihazPozisyon);
-
-        }, function () {
-            handleLocationError(true, infoWindow, map.getCenter());
-        });
-    } else {
-        // Browser doesn't support Geolocation
-        handleLocationError(false, infoWindow, map.getCenter());
-    }
-    console.log(map);
-}
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(browserHasGeolocation ?
-        'Error: The Geolocation service failed.' :
-        'Error: Your browser doesn\'t support geolocation.');
-}
-function cihazPozisyonAyarla(pozison) {
-    addMarker(pozison);
-}
-function addMarker(location) {
-    var marker = new google.maps.Marker({
-        position: location,
-        map: map
+﻿function harita() {
+    var markers = [];
+     
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: -34.397, lng: 150.644 },
+        scrollwheel: false,
+        disableDefaultUI: true,
+        zoom: 10
     });
-    map.setCenter(location);
-    markers.push(marker);
-    return marker;
-}
+   
+    this.cihazinYerinigoster = function () {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                var cihazPozisyon = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
+                map = new google.maps.Map(document.getElementById('map'), {
+                    center: cihazPozisyon,
+                    disableDefaultUI: true,
+                    zoom: 16
+                });
+                cihazPozisyonAyarla(cihazPozisyon);
+            }, function () {
+                handleLocationError(true, infoWindow, map.getCenter());
+            });
+        } else {
+            // Browser doesn't support Geolocation
+            handleLocationError(false, infoWindow, map.getCenter());
+        }
+
+        console.log(map);
+
+    };   
+
+
+    this.cihazPozisyonGuncelle = function(pozisyon) {
+        deleteMarkers();
+
+        cihazPozisyonAyarla(pozisyon);
+    };
+
+    // Sets the map on all markers in the array.
+    function setMapOnAll(map) {
+        for (var i = 0; i < markers.length; i++) {
+            markers[i].setMap(map);
+        }
+    }
+
+    // Removes the markers from the map, but keeps them in the array.
+    function clearMarkers() {
+        setMapOnAll(null);
+    }
+
+
+    function deleteMarkers() {
+        clearMarkers();
+        markers = [];
+    }
+
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+            'Error: The Geolocation service failed.' :
+            'Error: Your browser doesn\'t support geolocation.');
+    }
+    function cihazPozisyonAyarla(pozison) {
+        addMarker(pozison);
+    }
+    function addMarker(location) {
+        var marker = new google.maps.Marker({
+            position: location,
+            map: map
+        });
+        map.setCenter(location);
+        markers.push(marker);
+        return marker;
+    }
+
+    return this;
+
+} 
+
